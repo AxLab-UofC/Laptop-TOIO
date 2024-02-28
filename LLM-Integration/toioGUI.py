6d25de29f3ffafca557724ba8fcd9105f54577e5
Matthew Jeung <mjeung@Matthews-Laptop.local>
2024-02-28 14:32:56 -0600

    lebron

--- LLM-Integration/toioGUI.py
+++ LLM-Integration/toioGUI.py
@@ -7,29 +7,15 @@ import re
 import time
 import pygame
 
-#Dummmy data
-dummy_toio_positions = [{'id': 0, 'x': 0, 'y': 0, 'theta': 130},
-                         {'id': 1, 'x': 1, 'y': 1, 'theta': 208},
-                         {'id': 2, 'x': 1, 'y': 0, 'theta': 208},
-                         {'id': 3, 'x': 0, 'y': 1, 'theta': 208}]
-
-dummy_send = '[{"id": 0, "x": 125, "y": 288, "theta": 65}, {"id": 1, "x": 200, "y": 273, "theta": 61}, {"id": 2, "x": 210, "y": 205, "theta": 68}, {"id": 3, "x": 134, "y": 215, "theta": 59}]{"id": 0, "x": 125, "y": 288, "theta": 65}, {"id": 1, "x": 200, "y": 273, "theta": 61}, {"id": 2, "x": 210, "y": 205, "theta": 68}, {"id": 3, "x": 134, "y": 215, "theta": 59}]'
-
 #Set up Assistant
 gpt = OpenAI(
    api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-animation_thread = None #flag for if an animation is running in the background
-delay = 0.2
-
-#message_counter = 0
-
 assistant = gpt.beta.assistants.create(
     name="2D Graphed Image Interpreter",
-    instructions="You are a interpeter of a 2D graph. Given a list of points and connections between points\
-    , you are meant to interpret what basic images or shapes they might represent. The 2D plane has these specifications:\
-    the top left is (x=45,y=45), and the bottom right is (x=455,y=455).",
+    instructions="You are a interpeter of a 2D graph. Given a list of grouped points, you are meant to interpret what basic images or shapes they might represent. The 2D plane has these specifications:\
+    the top left corner of our plane is (x=45,y=45), and the bottom right corner is (x=455,y=455). You will also be asked to return altered sets of points, such that basic animations are created",
     tools=[{"type": "retrieval"}],
     model="gpt-4-0125-preview"
 )
@@ -42,6 +28,20 @@ client = udp_client.SimpleUDPClient("127.0.0.1", 4444)
 global_toio_positions = []
 user_input = ''
 submitted = 0
+playing = False
+reset = False
+animation_thread = None #flag for if an animation is running in the background
+delay = 0.4
+new_locations = None
+
+
+def save_file(string_data, filename, folder_path = "animations"):
+    os.makedirs(folder_path, exist_ok=True)
+    file_path = os.path.join(folder_path, filename)
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(string_data)
+    print(f"File saved successfully at {file_path}")
+
 
 def handle_toio_positions(unused_addr, *args):
     global global_toio_positions
@@ -130,11 +130,12 @@ def new_movements(animation_prompt):
     """
     Generate new positions given interpretation
     """
+    global new_locations
     #num_frames = input("How many frames?\n")
-    prompt = f"Based on your current understanding of the current positions, we want to generate a simple looping \
-         animation based on the following prompt: {animation_prompt}. Rearrange the points 4 times to form a simple \
-        looping animation with 4 frames. The response should given as 4 frames, each within [], and delimited \
-        by a semicolon ; (no spaces). Each frame should be in the same format as the positions were given to you. Only return the list of frames\
+    prompt = f"Based on your current understanding of the current shape, we want to generate a simple looping \
+        animation based on the following prompt: {animation_prompt}. If relevant, recall the grouping of our points and what each of these groups might represent. Use this understanding to inform the each rearrangement of points. \
+        Treat each frame as an adjustment of the original shape through moving each point. Not every point must move at each frame, only move the ones that make sense to move. Rearrange the points 4 times to form a simple looping \
+        animation with 4 frames. The response should given as 5 frames, each within [], and delimited by a semicolon ; (no spaces). Each frame should be in the same format as the positions were given to you. Only return the list of frames\
         with no extra words or description."
     if(animation_prompt != ''):
         new_locations = complete_one_prompt(prompt)
@@ -147,6 +148,8 @@ def start_animation(target, frames):
     Start animation thread
     """
     global animation_thread
+    global playing
+    playing = True
     if animation_thread is None:
         animation_thread = threading.Thread(target=target, args=[frames])
         animation_thread.start()
@@ -161,20 +164,31 @@ def animate_movements(frames):
     Run animations in the background
     """
     global delay
+    global playing
+    global reset
+
     i = 0
     while True:
-        try:
-            client.send_message("/new_positions", frames[i])
-            i += 1
-            if (i == len(frames)):
-                i = 0
-        except ValueError as e:
-            print(str(e))
+        if playing:
+            try:
+                client.send_message("/new_positions", frames[i])
+                i += 1
+                if (i == len(frames)):
+                    i = 0
+            except ValueError as e:
+                print(str(e))
+        if reset:
+            reset = False
+            break
         time.sleep(delay)
 
 
 def main():
     global animation_thread
+    global new_locations
+    global playing
+    global reset
+    running = True
     user_input = ''
     global submitted
     disp = dispatcher.Dispatcher()
@@ -194,24 +208,45 @@ def main():
     pygame.display.set_caption('Shape n Swarm')
     color_active = pygame.Color('lightskyblue3')
     current_step = "interpretation"
-    display = pygame.display.set_mode([600,600])
+    logo = pygame.transform.scale(pygame.image.load \
+            ('logo.png'), (200,100))
+    display = pygame.display.set_mode([800,600])
     font = pygame.font.Font('freesansbold.ttf', 20)
-    font2 = pygame.font.Font('freesansbold.ttf', 15)
     font3 = pygame.font.Font('freesansbold.ttf', 30)
+    font4 = pygame.font.Font('freesansbold.ttf', 40)
     result = "None"
     clock = pygame.time.Clock()
-    input_rect = pygame.Rect(50,150,140,20)
-    red = (255,0,0)
+    input_rect = pygame.Rect(50,300,140,35)
     box_active = False
-    buttonText = font3.render("Send Input", True, red, (255,255,0))
+    buttonText = font3.render("Send Instructions", True, (0,0,0), (60,179,113))
     buttonRect = buttonText.get_rect()
-    buttonRect.center = (300,200)
-    while True:
+    buttonRect.center = (400,410)
+    saveText = font.render("Save", True, (0,0,0), (60,179,113))
+    saveRect = saveText.get_rect()
+    saveRect.center = (300,450)
+    resetText = font.render("Reset", True, (0,0,0), (60,179,113))
+    resetRect = resetText.get_rect()
+    resetRect.center = (400,450)
+    if playing:
+        playText = font.render("Pause", True, (0,0,0), (60,179,113))
+    else:
+        playText = font.render("Play", True, (0,0,0), (60,179,113))
+    playRect = playText.get_rect()
+    playRect.center = (500,450)
+    e1Text = font.render("Example 1: Walking", True, (0,0,0), (60,179,113))
+    e1Rect = e1Text.get_rect()
+    e1Rect.center = (50,550)
+    e2Text = font.render("Example 2: Expressions", True, (0,0,0), (60,179,113))
+    e2Rect = e2Text.get_rect()
+    e2Rect.center = (50,550)
+    e3Text = font.render("Example 3: Walking", True, (0,0,0), (60,179,113))
+    e3Rect = e3Text.get_rect()
+    e3Rect.center = (50,550)
+    #Main loop
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                server_thread.join()
-                break
+                running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if input_rect.collidepoint(event.pos):
                     box_active = True
@@ -220,9 +255,33 @@ def main():
                         result = interpret_toios(user_input, global_toio_positions)
                         current_step = "animation"
                         user_input = ''
+                        filename = result.split(" ")[0]
+                        filename = filename + ".txt"
+                        print(filename)
                     else:
                         new_movements(user_input)
                         user_input = ''
+                elif saveRect.collidepoint(event.pos):
+                    if new_locations is not None and filename is not None:
+                        save_file(new_locations, filename)
+                elif playRect.collidepoint(event.pos):
+                    if playing:
+                        playing = False
+                    else:
+                        playing = True
+                elif resetRect.collidepoint(event.pos):
+                    reset = True
+                    playing = False
+                    animation_thread.join()
+                    animation_thread = None
+                    current_step = "interpretation"
+                    result = None
+                    user_input = ''
+                    filename = None
+                elif e1Rect.collidepoint(event.pos):
+                    tmp = json.load("animations/walking.txt")
+                    frames = tmp.split(";")
+                    start_animation(frames, )
                 else:
                     box_active = False
             if event.type == pygame.KEYDOWN:
@@ -237,35 +296,39 @@ def main():
             color = (200,200,200)
         display.fill((255,255,255))
         pygame.draw.rect(display, color, input_rect)
-        text_surface = font2.render(user_input, True, (0,0,0))
+        text_surface = font.render(user_input, True, (0,0,0))
         display.blit(text_surface, (input_rect.x+5, input_rect.y+5))
-        input_rect.w = max(500, text_surface.get_width()+10)
-        instructionsText = font.render("Please input instructions for the {}".format(current_step), True, (0,0,0), (255,255,255))
+        display.blit(logo, (600,0))
+        input_rect.w = max(700, text_surface.get_width()+10)
+        instructionsText = font.render("Please input instructions for the {} of your design".format(current_step), True, (0,0,0), (255,255,255))
         instructionsRect = instructionsText.get_rect()
-        instructionsRect.center = (300,100)
-        outText = font3.render("Interpretation: {}".format(result), True, (0,0,0), (255,255,255))
+        instructionsRect.center = (400,280)
+        outText = font.render("Current interpretation of your design: {}".format(result), True, (0,0,0), (255,255,255))
         outRect = outText.get_rect()
-        outRect.center = (300,50)
+        outRect.center = (400,250)
+        titleText = font4.render("Shape n' Swarm", True, (0,0,0), (255,255,255))
+        titleRect = titleText.get_rect()
+        titleRect.center = (400, 100)
+        descriptionText = font.render("Smart Swarm Robots for Creative Interaction and Play Using LLMs", True, (0,0,0), (255,255,255))
+        descriptionRect = descriptionText.get_rect()
+        descriptionRect.center = (400, 150)
+        display.blit(descriptionText, descriptionRect)
+        display.blit(titleText, titleRect)
         display.blit(buttonText, buttonRect)
         display.blit(outText, outRect)
         display.blit(instructionsText, instructionsRect)
+        display.blit(e1Text, e1Rect)
+        display.blit(e2Text, e2Rect)
+        display.blit(e3Text, e3Rect)
+        if animation_thread is not None:
+            display.blit(saveText, saveRect)
+            display.blit(playText, playRect)
+            display.blit(resetText, resetRect)
         pygame.display.update()
-        clock.tick(60)
-    
-    return
-    # user_message = input("Add further information to interpret toio positions (optional): ")
-    # result = interpret_toios(user_message, global_toio_positions)
-    # print(result)
-    # while(True):
-    #     update_locations = input("Would you like to invoke movement? (Enter if no): ")
-    #     if(update_locations != ''):
-    #         new_movements()
-    #     else:
-    #         if animation_thread:
-    #             animation_thread.join()
-    #         break
-    
+        clock.tick(60)  
     server_thread.join()
+    pygame.display.quit()
+    pygame.quit()
     return
 
 if __name__ == "__main__":
